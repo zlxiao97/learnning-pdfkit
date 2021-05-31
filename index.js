6933afaf78df54bdc1946ed3f4e2bc04a9ef43e2
肖子立 <xiaozlb@digitalchina.com>
2021-05-31 22:56:16 +0800

    合同更新

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const TEXT_BLUE_COLOR = [0, 0, 255]
 
 /** 表格数据 */
 const formData = require('./data/form.json')
-const DOC_POSTION_X = formData.startX
-const DOC_POSTION_Y = formData.startY
+
 const DOC_WIDTH = formData.width
 const DOC_MAX_COLUMNS = 24
 
@@ -42,7 +41,11 @@ const setCheckBox = ({ text, positionX, positionY, gap = 2, boxSize = 10, color,
 }
 
 /** 表格绘制辅助方法 */
-const drawRect = ({ offsetX = 0, offsetY = 0, column = DOC_MAX_COLUMNS, rowHeight }) => doc.strokeColor('red').rect(getX(DOC_POSTION_X + offsetX), getY(DOC_POSTION_Y + offsetY), getX(column / DOC_MAX_COLUMNS * DOC_WIDTH), getY(rowHeight)).stroke()
+const drawRect = ({ offsetX = 0, offsetY = 0, column = DOC_MAX_COLUMNS, rowHeight, page }) => {
+  const DOC_POSTION_X = page.startX
+  const DOC_POSTION_Y = page.startY
+  doc.strokeColor('red').rect(getX(DOC_POSTION_X + offsetX), getY(DOC_POSTION_Y + offsetY), getX(column / DOC_MAX_COLUMNS * DOC_WIDTH), getY(rowHeight)).stroke()
+}
 
 /** 创建 PDF 给定文档配置 */
 const doc = new PDFDocument({
@@ -54,63 +57,41 @@ const doc = new PDFDocument({
 /** 文件存储位置 */
 doc.pipe(fs.createWriteStream(path.resolve(__dirname, './dist/demo.pdf')))
 
-/** 底图：空白合同 */
-doc.image('img/demo_empty.jpg', {
-  width: A4_WIDTH,
-  height: A4_HEIGHT,
-  align: 'center',
-  valign: 'center'
-})
 
-/** 绘制表格 */
-formData.rows.reduce((lastOffsetY, row) => {
-  drawRect({ offsetX: 0, offsetY: lastOffsetY, rowHeight: row.height })
-  return lastOffsetY + row.height
-}, 0)
+/** 逐页绘制 */
+formData.pages.forEach((page, index) => {
+  if (index) {
+    doc.addPage()
+  }
+  /** 底图：空白合同 */
+  doc.image(`img/page${index + 1}.jpg`, {
+    width: A4_WIDTH,
+    height: A4_HEIGHT,
+    align: 'center',
+    valign: 'center'
+  })
+  /** 绘制表格 */
+  page.rows.reduce((lastOffsetY, row) => {
+    drawRect({ offsetX: 0, offsetY: lastOffsetY, rowHeight: row.height, page })
+    return lastOffsetY + row.height
+  }, 0)
+})
 
 
 /** 插入数据 */
-setText({
-  text: '熊巍',
-  positionX: 32,
-  positionY: 15.8,
-  color: TEXT_RED_COLOR
-})
+// setText({
+//   text: '熊巍',
+//   positionX: 32,
+//   positionY: 15.8,
+//   color: TEXT_RED_COLOR
+// })
 
-setText({
-  text: 'WYB4202021529032036',
-  positionX: 25,
-  positionY: 12.8,
-  color: TEXT_RED_COLOR
-})
-
-setText({
-  text: '2021/5/29',
-  positionX: 83.2,
-  positionY: 12.8,
-  color: TEXT_RED_COLOR
-})
-
-setText({
-  text: '695668628@qq.com',
-  positionX: 75,
-  positionY: 18.5,
-  color: TEXT_RED_COLOR
-})
-
-setText({
-  text: '100,000.00',
-  positionX: 29.5,
-  positionY: 46.8,
-  color: TEXT_BLUE_COLOR
-})
-
-setCheckBox({
-  text: '家庭自用',
-  positionX: 75,
-  positionY: 38.3,
-  color: TEXT_BLUE_COLOR
-})
+// setCheckBox({
+//   text: '家庭自用',
+//   positionX: 75,
+//   positionY: 38.3,
+//   color: TEXT_BLUE_COLOR
+// })
 
 
 doc.end();
