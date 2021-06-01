6a9951ef046aa0410f8ff972ba61aec46b5dca01
肖子立 <xiaozlb@digitalchina.com>
2021-06-01 23:05:55 +0800

    填入数据

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs')
 const path = require('path');
 
+/** demo 数据 */
+const demoData = require('./demo/data.json')
+
 
 /** PDF 页面A4纸打印宽高 */
 const A4_WIDTH = 595.28;
@@ -11,6 +14,7 @@ const A4_HEIGHT = 841.89;
 const TEXT_SIZE = 10
 const TEXT_RED_COLOR = [192, 80, 77]
 const TEXT_BLUE_COLOR = [0, 0, 255]
+const TEXT_BLACK_COLOR = [0, 0, 0]
 
 /** 表格数据 */
 const formData = require('./data/form.json')
@@ -27,7 +31,6 @@ const getPercentY = y => y * 100 / A4_HEIGHT
 /** 数据插入辅助方法 */
 const setText = ({ text, positionX, positionY, color, size = TEXT_SIZE }) => doc
   .fillColor(color)
-  .fontSize(size)
   .text(text, getX(positionX), getY(positionY));
 
 const setCheckBox = ({ text, positionX, positionY, gap = 2, boxSize = 10, color, size = TEXT_SIZE }) => {
@@ -41,10 +44,27 @@ const setCheckBox = ({ text, positionX, positionY, gap = 2, boxSize = 10, color,
 }
 
 /** 表格绘制辅助方法 */
-const drawRect = ({ offsetX = 0, offsetY = 0, column = DOC_MAX_COLUMNS, rowHeight, page }) => {
-  const DOC_POSTION_X = page.startX
-  const DOC_POSTION_Y = page.startY
-  doc.strokeColor('red').rect(getX(DOC_POSTION_X + offsetX), getY(DOC_POSTION_Y + offsetY), getX(column / DOC_MAX_COLUMNS * DOC_WIDTH), getY(rowHeight)).stroke()
+const drawRect = ({ offsetX = 0, offsetY = 0, column = DOC_MAX_COLUMNS, color = "red", rowHeight, page }) => {
+  const docPostionX = page.startX
+  const docPositionY = page.startY
+  doc.strokeColor(color).rect(getX(docPostionX + offsetX), getY(docPositionY + offsetY), getX(column / DOC_MAX_COLUMNS * DOC_WIDTH), getY(rowHeight)).stroke()
+}
+
+/** 文本垂直水平居中辅助方法 */
+const getCenteredPostion = ({ offsetX = 0, offsetY = 0, column = DOC_MAX_COLUMNS, rowHeight, page, text }) => {
+  const docPostionX = page.startX
+  const docPositionY = page.startY
+  const containerX = docPostionX + offsetX
+  const containerY = docPositionY + offsetY
+  const containerWidth = column / DOC_MAX_COLUMNS * DOC_WIDTH
+  const containerHeight = rowHeight
+  const textWidth = getPercentX(doc.widthOfString(text))
+  const textHeight = getPercentY(doc.heightOfString(text))
+  return {
+    positionX: containerX + 0.2,
+    positionY: containerY + containerHeight / 2 - textHeight / 2,
+  }
+
 }
 
 /** 创建 PDF 给定文档配置 */
@@ -53,6 +73,7 @@ const doc = new PDFDocument({
   font: 'fonts/puhui/Alibaba-PuHuiTi-Medium.ttf',
   size: 'A4'
 });
+doc.fontSize(TEXT_SIZE)
 
 /** 文件存储位置 */
 doc.pipe(fs.createWriteStream(path.resolve(__dirname, './dist/demo.pdf')))
@@ -72,26 +93,27 @@ formData.pages.forEach((page, index) => {
   })
   /** 绘制表格 */
   page.rows.reduce((lastOffsetY, row) => {
-    drawRect({ offsetX: 0, offsetY: lastOffsetY, rowHeight: row.height, page })
+    // drawRect({ offsetX: 0, offsetY: lastOffsetY, rowHeight: row.height, page }) //更新表格定位用代码
+    if (row.fields) {
+      row.fields.forEach(field => {
+        // drawRect({ offsetX: field.startX, offsetY: lastOffsetY, rowHeight: row.height, column: field.column, color: 'blue', page }) //更新表格定位用代码
+        /** 插入数据 */
+        if (field.dataIndex) {
+          const fieldData = demoData[field.dataIndex]
+          const position = getCenteredPostion({ offsetX: field.startX, offsetY: lastOffsetY, rowHeight: row.height, column: field.column, page, text: fieldData })
+          setText({
+            text: fieldData,
+            color: TEXT_BLACK_COLOR,
+            ...position
+          })
+        }
+      })
+    }
     return lastOffsetY + row.height
   }, 0)
 })
 
 
-/** 插入数据 */
-// setText({
-//   text: '熊巍',
-//   positionX: 32,
-//   positionY: 15.8,
-//   color: TEXT_RED_COLOR
-// })
-
-// setCheckBox({
-//   text: '家庭自用',
-//   positionX: 75,
-//   positionY: 38.3,
-//   color: TEXT_BLUE_COLOR
-// })
 
 
 doc.end();
